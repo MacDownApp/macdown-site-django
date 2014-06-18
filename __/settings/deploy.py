a8217f3a030c0104628ef7b5be15617984e1dc87
Tzu-ping Chung <uranusjr@gmail.com>
2014-06-18 23:53:44 +0800

    Add secret key

--- __/settings/deploy.py
+++ __/settings/deploy.py
@@ -1,10 +1,12 @@
 from .base import *     # noqa
-
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+DATABASES['default'] = dj_database_url.config()
 
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = 'static'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
