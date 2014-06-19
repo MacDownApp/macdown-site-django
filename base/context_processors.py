32611f2459b7415c68f3f13464685efb89a5adaa
Tzu-ping Chung <uranusjr@gmail.com>
2014-06-19 17:00:21 +0800

    Explicitly specify latest key

--- base/context_processors.py
+++ base/context_processors.py
@@ -5,7 +5,7 @@ def latest_version(request):
     try:
         latest_version = Version.objects.filter(
             application__slug='macdown'
-        ).latest()
+        ).latest('publish_at')
     except Version.DoesNotExist:
         latest_version = None
     return {'latest_version': latest_version}
