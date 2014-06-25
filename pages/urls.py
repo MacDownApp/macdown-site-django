39e93d70b905e6bbecbab4a99b199d424ad0446a
Tzu-ping Chung <uranusjr@gmail.com>
2014-06-25 17:54:26 +0800

    More FAQ and seperate it to another page

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.home, name='home'),
+    url(r'^faq/$', views.faq, name='faq'),
 )
--- pages/views.py
+++ pages/views.py
@@ -5,4 +5,9 @@ class HomeView(TemplateView):
     template_name = 'pages/home.html'
 
 
+class FAQView(TemplateView):
+    template_name = 'pages/faq.html'
+
+
 home = HomeView.as_view()
+faq = FAQView.as_view()
