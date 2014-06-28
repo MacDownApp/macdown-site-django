506aa4c46a7b73d31a72e2346ad2dd25335512a4
Tzu-ping Chung <uranusjr@gmail.com>
2014-06-29 04:06:04 +0800

    Add features page
    Fix #2

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.home, name='home'),
+    url(r'^features/$', views.features, name='features'),
     url(r'^faq/$', views.faq, name='faq'),
 )
--- pages/views.py
+++ pages/views.py
@@ -5,9 +5,14 @@ class HomeView(TemplateView):
     template_name = 'pages/home.html'
 
 
+class FeaturesView(TemplateView):
+    template_name = 'pages/features.html'
+
+
 class FAQView(TemplateView):
     template_name = 'pages/faq.html'
 
 
 home = HomeView.as_view()
+features = FeaturesView.as_view()
 faq = FAQView.as_view()
