82b384ec16ee57edd48f8ebfc3455b1c3f18ab39
Tzu-ping Chung <uranusjr@gmail.com>
2014-07-04 18:46:10 +0800

    Better bundle version URL schema

--- downloads/urls.py
+++ downloads/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns(
     url(r'^latest/$', views.latest, name='latest'),
     url(r'^v(?P<short_version>\d+(?:\.\w+)+)/$', views.version,
         name='short_version'),
-    url(r'^(?P<version>\d+)/$', views.version, name='version'),
+    url(r'^(?P<version>[\.\d]+)/$', views.version, name='version'),
 )
