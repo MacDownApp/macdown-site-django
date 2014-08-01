005a634ba3e646dd410ed99b81d94135b9c23a11
Tzu-ping Chung <uranusjr@gmail.com>
2014-08-01 15:45:16 +0800

    Implement post caching

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
+import os
 import mistune
 from django.http import Http404, HttpResponsePermanentRedirect
 from django.views.generic import TemplateView
 from .posts import Post, PostDoesNotExist, get_post_filelist, get_post_filename
 from .utils import Renderer, resolve_prism_languages
+
+
+# id => post instance
+_post_cache = {}
 
 
 class PostListView(TemplateView):
@@ -13,9 +18,15 @@ class PostListView(TemplateView):
         posts = []
         for filename in reversed(get_post_filelist()):
             try:
-                post = Post(filename)
-            except PostDoesNotExist:
-                continue
+                base, _ = os.path.splitext(filename)
+                post_id = int(Post.FILENAME_PATTERN.match(base).group(1))
+                post = _post_cache[post_id]
+            except (AttributeError, KeyError):
+                try:
+                    post = Post(filename)
+                except PostDoesNotExist:
+                    continue
+                _post_cache[post.id] = post
             if post.title:
                 posts.append(post)
         data = super().get_context_data()
@@ -28,11 +39,16 @@ class PostDetailView(TemplateView):
     template_name = 'blog/post_detail.html'
 
     def get(self, request, *args, **kwargs):
+        post_id = int(kwargs['id'])
         post_slug = kwargs.get('slug')
         try:
-            post = Post(get_post_filename(id=kwargs['id']))
-        except PostDoesNotExist:
-            raise Http404
+            post = _post_cache[post_id]
+        except KeyError:
+            try:
+                post = Post(get_post_filename(id=post_id))
+            except PostDoesNotExist:
+                raise Http404
+            _post_cache[post_id] = post
         if post.slug != post_slug:
             canonical_url = post.get_absolute_url()
             return HttpResponsePermanentRedirect(redirect_to=canonical_url)
