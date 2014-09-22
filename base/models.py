21b5ee062efeb162fa20ab772923072325b5bc6a
Tzu-ping Chung <uranusjr@gmail.com>
2014-09-22 13:49:19 +0800

    Make sure things don't crash with MacDown app

--- base/models.py
+++ base/models.py
@@ -2,4 +2,7 @@ from django.utils.functional import SimpleLazyObject
 from sparkle.models import Application
 
 
-macdown = SimpleLazyObject(lambda: Application.objects.get(slug='macdown'))
+def get_macdown():
+    return Application.objects.get_or_create(slug='macdown')[0]
+
+macdown = SimpleLazyObject(get_macdown)
